9f8e179ff29d6f41645911d29ae7d56b8a795694
Alex Noir <Varsash@Gmail.com>
2017-04-01 19:46:13 +0300

    init

---
+++ server.cs
@@ -0,0 +1,15 @@
+datablock PlayerData(PlayerDFArmor : PlayerStandardArmor)
+{
+	//shapeFile = "base/data/shapes/player/m_ds.dts";
+	uiName = "Despair Fever Player";
+
+	cameraMaxDist = 2;
+	cameraVerticalOffset = 1.25;
+	maxFreelookAngle = 2;
+
+	canJet = 0;
+	mass = 120;
+	//maxTools = 5;
+
+	jumpForce = 1200;
+};
