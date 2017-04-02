4006452308e99658ec8f47c89d8ee3f53c87e630
Alex Noir <Varsash@Gmail.com>
2017-04-02 21:54:38 +0300

    i've been doing nothing but random experimental shit

--- src/weapons/bat.cs
+++ src/weapons/bat.cs
@@ -1,7 +1,7 @@
-datablock ItemData(BaseballItem)
+datablock ItemData(BatItem)
 {
-	shapeFile = $Despair::Path @ "res/shapes/weapons/baseballbat.dts";
-	image = BaseballImage;
+	shapeFile = $Despair::Path @ "res/shapes/weapons/bat.dts";
+	image = BatImage;
 	mass = 1;
 	drag = 0.3;
 	density = 0.2;
@@ -9,62 +9,41 @@ datablock ItemData(BaseballItem)
 	friction = 1;
 	doColorShift = true;
 	colorShiftColor = "0.47 0.35 0.2 1";
+	uiName = "Baseball Bat";
+	canDrop = true;
 };
 
-datablock ShapeBaseImageData(BaseballImage)
+function BatItem::onAdd(%this, %obj)
 {
-	shapeFile = $Despair::Path @ "res/shapes/weapons/baseballbat.dts";
-	item = BaseballItem;
+	parent::onAdd(%this, %obj);
+	%obj.playThread(0, "root");
+}
+
+datablock ShapeBaseImageData(BatImage)
+{
+	className = "WeaponImage";
+	item = BatItem;
+
+	shapeFile = $Despair::Path @ "res/shapes/weapons/bat.dts";
 	doColorShift = true;
 	colorShiftColor = "0.47 0.35 0.2 1";
 
-	name = "Baseball Bat";
-	type = "melee";
-	fireDelay = 0.6;
-	moveSpeed = 0.95;
-	points = 800;
-	blunt = true;
-	canExecute = true;
-	pickup = true;
-	meleeRange = 4;
+	type = "blunt";
+
+	raycastEnabled = 1;
+	raycastRange = 4;
+	raycastFromEye = true;
+
+	stateName[0]					= "Activate";
+	stateAllowImageChange[0]		= 1;
+	stateSequence[0]				= "root";
+	stateTransitionOnAmmo[0]		= "Blood";
+
+	stateName[2]					= "Blood";
+	stateSequence[2]				= "blood";
 };
 
-function BaseballImage::onMount(%image, %player, %slot)
+function BatImage::onMount(%image, %player, %slot)
 {
-	%player.lastMeleeSide = 0;
-	%player.playThread(1, swing1);
-	%player.schedule(32, stopThread, 1);
-}
-
-function BaseballImage::onFire(%image, %player)
-{
-	%player.playThread(1, %player.lastMeleeSide ? swing2 : swing1);
-
-	%player.lastMeleeSide = !%player.lastMeleeSide;
-	%player.lastFireTime = $Sim::Time;
-
-	play3D(MeleeMediumSwing @ getRandom(1, 4), %player.getSlotTransform(0), %player.client);
-	return fireMelee(%image, %player);
-}
-
-function BaseballImage::onMeleeHit(%image, %player, %object, %position, %normal)
-{
-	if (!isObject(%object))
-		return;
-	
-	if (%object.getType() & $TypeMasks::PlayerObjectType)
-	{
-		play3D(BaseballBatHitPlSound @ getRandom(1, 2), %position);
-		sprayBloodWide(%position, VectorScale(%normal, 6));
-		%damage = 1;
-		if (%player.inManualExecution)
-		{
-			%damage = 0.35;
-			cancel(%player.executionSchedule);
-			%player.executionSchedule = %player.schedule(1000, executeManualTimeout);
-		}
-		return %object.damage(%damage, %player.client, %image, %position);
-	}
-
-	play3D(BaseballBatHitEnvSound @ getRandom(1, 2), %position);
+	%player.setImageAmmo(%slot, %player.bloody);
 }
