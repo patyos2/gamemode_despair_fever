a9c68167eb2c03d9d3152325a525043c494f968c
patyos2 <99589776+patyos2@users.noreply.github.com>
2025-05-31 23:03:44 -0400

    Revolver Fix innocent -> role transfer with shooting in the body from bleeding.
    (Monoblaster)Discovered issue with innocent -> role transfer with shooting in the body from bleeding.
    (patyos) Just a small nerf to fiber strand dropping with revolver 30% -> 40%

--- src/weapons/revolver.cs
+++ src/weapons/revolver.cs
@@ -200,7 +200,7 @@ function GenericBulletWeapon::onFire(%image, %player)
     if (%player.client)
         %player.client.updateBottomPrint();
 	
-	if(getRandom() < 0.3)
+	if(getRandom() < 0.4)
 		%player.spawnFiber();
     return true;
 }
@@ -273,7 +273,7 @@ function FeverProjectile::onCollision(%data, %projectile, %object, %fade, %posit
     }
 
     serverPlay3d("ImpactFleshSound", %position);
-    %object.damage(%projectile, %position, %projectile.damage, %projectile.image.type);
+    %object.damage(%projectile.source.player, %position, %projectile.damage, %projectile.image.type);
     sprayBloodFromHit(%position, VectorScale(%velocity, 0.2), %object);
 }
 
